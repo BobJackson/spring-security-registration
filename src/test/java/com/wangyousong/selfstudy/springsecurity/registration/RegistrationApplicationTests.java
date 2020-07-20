188284b2f42239321a2bdbf3a4cf5ed09d353d66
BobJackson <1187688895@qq.com>
2020-07-20 14:22:28 +0800

    Find the Registered Spring Security Filters

--- src/test/java/com/wangyousong/selfstudy/springsecurity/registration/RegistrationApplicationTests.java
+++ src/test/java/com/wangyousong/selfstudy/springsecurity/registration/RegistrationApplicationTests.java
@@ -1,13 +1,35 @@
 package com.wangyousong.selfstudy.springsecurity.registration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.Filter;
+import java.util.List;
 
 @SpringBootTest
 class RegistrationApplicationTests {
 
+    @Autowired
+    @Qualifier("springSecurityFilterChain")
+    private Filter springSecurityFilterChain;
+
+
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    public void getFilters() {
+        FilterChainProxy filterChainProxy = (FilterChainProxy) springSecurityFilterChain;
+        List<SecurityFilterChain> list = filterChainProxy.getFilterChains();
+        list.stream()
+                .flatMap(chain -> chain.getFilters().stream())
+                .forEach(filter -> System.out.println(filter.getClass()));
     }
 
 }
